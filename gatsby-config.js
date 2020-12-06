5e3f6bf76a6a04bb5951844b995a232b2ad0d914
Mr Yerena <mryerena86@gmail.com>
2020-12-06 14:15:46 -0600

    Added initial packages to the project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const globImporter = require('node-sass-glob-importer')
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -6,6 +8,13 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-resolve-src`,
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        importer: globImporter(),
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
